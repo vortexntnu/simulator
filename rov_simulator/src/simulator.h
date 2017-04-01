250473c59441260055fa47fb04a2f2dd911d7cf6
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2017-04-01 10:15:39 +0200

    Change any tabs to spaces

--- rov_simulator/src/simulator.h
+++ rov_simulator/src/simulator.h
@@ -15,24 +15,24 @@ typedef std::vector<double> stdvec;
 
 class Simulator {
   public:
-  	Simulator(unsigned int f,
+    Simulator(unsigned int f,
               ros::NodeHandle nh);
-  	void thrustCallback(const vortex_msgs::Float64ArrayStamped &msg);
+    void thrustCallback(const vortex_msgs::Float64ArrayStamped &msg);
    void spin();
   private:
     void poseArmaToMsg(const arma::vec &e,
                        geometry_msgs::Pose &m);
     void twistArmaToMsg(const arma::vec &e,
                         geometry_msgs::Twist &m);
-  	unsigned int    frequency;
-  	ros::NodeHandle nh;
-  	ros::Subscriber wrenchSub;
+    unsigned int    frequency;
+    ros::NodeHandle nh;
+    ros::Subscriber wrenchSub;
     ros::Publisher  posePub;
     ros::Publisher  twistPub;
     ros::Publisher  imuPub;
     ros::Publisher  pressurePub;
-  	Dynamics        *dynamics;
-  	arma::vec       u;
+    Dynamics        *dynamics;
+    arma::vec       u;
 };
 
 #endif
