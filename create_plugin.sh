e40d95ae72f3bd2f461d105a4cc71fe96eae3f53
olesot <ole.sivert.otterholm@gmail.com>
2018-01-21 11:26:19 +0100

    Strucural changes. Add manipulator mounted rov model, not finished.

--- create_plugin.sh
+++
@@ -1,6 +0,0 @@
-mkdir build
-cd build
-cmake ..
-make
-
-mv libsimple_ROV_plugin.so $PWD/../model/rov/plugins/
---
+++ model/plugins/rov/rov_plugin.cc
@@ -0,0 +1,98 @@
+#include <boost/bind.hpp>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <thread>
+#include "ros/ros.h"
+#include "geometry_msgs/Wrench.h"
+
+namespace gazebo
+{
+  class Rov : public ModelPlugin
+  {
+    public: void Load(physics::ModelPtr _parent, sdf::ElementPtr)
+    {
+      this->model = _parent;
+      this->link = model->GetLink("link");
+      this->inertial = link->GetInertial();
+      this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+          boost::bind(&Rov::OnUpdate, this, _1));
+
+      if (!ros::isInitialized())
+      {
+        int argc = 0;
+        char **argv = NULL;
+        ros::init(argc, argv, "gazebo_client");
+      }
+
+      this->rosNode.reset(new ros::NodeHandle("gazebo_client"));
+      this->rosSub = this->rosNode->subscribe("/rov_forces", 10, &Rov::OnRosMsg, this);
+
+      this->linFrictionCoef  = math::Vector3(80,80,203);
+      this->angFrictionCoef = math::Vector3(1,1,1);
+      this->forceSign = math::Vector3(1,-1,1);
+      this->torqueSign = math::Vector3(1,-1,-1);
+    }
+
+
+    public: void OnUpdate(const common::UpdateInfo & /*_info*/)
+    {
+      this->link->AddRelativeForce(this->force*this->forceSign);
+      this->link->AddRelativeTorque(this->torque*this->torqueSign);
+
+      math::Vector3 linVel = this->link->GetRelativeLinearVel();
+      math::Vector3 angVel = this->link->GetRelativeAngularVel();
+      math::Vector3 linFriction = this->linFrictionCoef*linVel*linVel.GetAbs();
+      math::Vector3 angFriction = this->angFrictionCoef*angVel*angVel.GetAbs();
+
+      this->link->AddRelativeForce(-linFriction);
+      this->link->AddRelativeTorque(-angFriction);
+
+    }
+
+
+    public: void OnRosMsg(const geometry_msgs::Wrench &_msg)
+    {
+      math::Vector3 force_msg(_msg.force.x, _msg.force.y, _msg.force.z);
+      math::Vector3 torque_msg(_msg.torque.x, _msg.torque.y, _msg.torque.z);
+
+      this->force = force_msg;
+      this->torque = torque_msg;
+
+      bool mode = this->link->GetGravityMode();
+
+      ROS_INFO("Force [%f,%f,%f], Torque [%f,%f,%f]",force.x, force.y, force.z,
+torque.x, torque.y, torque.z);
+      math::Vector3 force_debug = this->link->GetRelativeForce();
+      math::Vector3 torque_debug = this->link->GetRelativeTorque();
+
+      ROS_INFO("DBG_F [%f, %f, %f], DBG_T [%f,%f,%f]", force_debug.x,
+force_debug.y, force_debug.z, torque_debug.x, torque_debug.y, torque_debug.z);
+
+      ROS_INFO("GravityMode: %d", mode);
+    }
+
+
+    private: physics::ModelPtr model;
+    private: physics::LinkPtr link;
+    private: physics::InertialPtr inertial;
+    private: event::ConnectionPtr updateConnection;
+
+    private: std::unique_ptr<ros::NodeHandle> rosNode;
+    private: ros::Subscriber rosSub;
+
+    private: math::Vector3 force;
+    private: math::Vector3 torque;
+
+    private: math::Vector3 linFrictionCoef;
+    private: math::Vector3 angFrictionCoef;
+
+    private: math::Vector3 forceSign;
+    private: math::Vector3 torqueSign;
+  };
+
+  GZ_REGISTER_MODEL_PLUGIN(Rov)
+}
