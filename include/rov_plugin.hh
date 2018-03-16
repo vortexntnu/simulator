118a24f369dc49182f8b4c05b74ffdf2eaeca763
olesot <ole.sivert.otterholm@gmail.com>
2018-03-16 15:19:34 +0100

    Set thruster forces allocated to thruster position

--- include/rov_plugin.hh
+++ include/rov_plugin.hh
@@ -1,8 +1,13 @@
+#ifndef ROV_PLUGIN_HH
+#define ROV_PLUGIN_HH
+
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
 #include <gazebo/common/common.hh>
 
 #include "ros/ros.h"
+
+#include "vortex_msgs/ThrusterForces.h"
 #include "geometry_msgs/Wrench.h"
 #include "sensor_msgs/FluidPressure.h"
 
@@ -14,7 +19,11 @@ namespace gazebo
       void Load(physics::ModelPtr _parent, sdf::ElementPtr);
       void simulationCallback(const common::UpdateInfo &);
       void rovForceCallback(const geometry_msgs::Wrench &_msg);
+      void thrusterForceCallback(const vortex_msgs::ThrusterForces &_msg);
       void timerCallback(const ros::TimerEvent&);
+      void applyFrictionForces();
+      void applyRovForces();
+      void applyThrusterForces();
     private:
       physics::ModelPtr                 m_model;
       physics::LinkPtr                  m_link;
@@ -22,19 +31,52 @@ namespace gazebo
 
       std::unique_ptr<ros::NodeHandle>  m_nh;
       ros::Subscriber                   m_rovForcesSub;
+      ros::Subscriber                   m_thrusterForcesSub;
       ros::Publisher                    m_pressurePub;
       ros::Timer                        m_timer;
 
-      math::Vector3                     m_pose;
+      math::Pose                        m_pose;
 
       sensor_msgs::FluidPressure        m_pressure;
 
       math::Vector3                     m_force;
       math::Vector3                     m_torque;
 
+      std::vector<math::Vector3>        m_thrusterForces = {
+      math::Vector3(0, 0, 0),
+      math::Vector3(0, 0, 0),
+      math::Vector3(0, 0, 0),
+      math::Vector3(0, 0, 0),
+      math::Vector3(0, 0, 0),
+      math::Vector3(0, 0, 0),
+      math::Vector3(0, 0, 0),
+      math::Vector3(0, 0, 0)};
+
+      // ENU Convension (gazebo world reference frame)
+      const std::vector<math::Vector3>  c_thrusterLayout = {
+      math::Vector3( 0.70711,  0.70711,  0),
+      math::Vector3( 0.00000,  0.00000, -1),
+      math::Vector3( 0.00000,  0.00000, -1),
+      math::Vector3(-0.70711,  0.70711,  0),
+      math::Vector3(-0.70711, -0.70711,  0),
+      math::Vector3( 0.00000,  0.00000, -1),
+      math::Vector3( 0.00000,  0.00000, -1),
+      math::Vector3( 0.70711, -0.70711,  0)};
+      // ENU Convension (gazebo world reference frame)
+      const std::vector<math::Vector3>  c_thrusterPosition = {
+      math::Vector3( 0.7, -0.7, 0),
+      math::Vector3( 0.5, -0.5, 0),
+      math::Vector3(-0.5, -0.5, 0),
+      math::Vector3(-0.7, -0.7, 0),
+      math::Vector3(-0.7,  0.7, 0),
+      math::Vector3(-0.5,  0.5, 0),
+      math::Vector3( 0.5,  0.5, 0),
+      math::Vector3( 0.7,  0.7, 0)};
       const math::Vector3               c_linearDragCoeff = math::Vector3(80, 80, 204);
       const math::Vector3               c_angularDragCoeff = math::Vector3(1, 1, 1);
 
   };
   GZ_REGISTER_MODEL_PLUGIN(Rov)
 }
+
+#endif  // ROV_PLUGIN_HH
--- src/rov_plugin.cc
+++ src/rov_plugin.cc
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <thread>
 
-
 void gazebo::Rov::Load(physics::ModelPtr _parent, sdf::ElementPtr)
 {
   this->m_model = _parent;
@@ -22,6 +21,8 @@ void gazebo::Rov::Load(physics::ModelPtr _parent, sdf::ElementPtr)
   this->m_nh.reset(new ros::NodeHandle("gazebo_client"));
   this->m_rovForcesSub =
     this->m_nh->subscribe("/rov_forces", 10, &Rov::rovForceCallback, this);
+  this->m_thrusterForcesSub =
+    this->m_nh->subscribe("/thruster_forces", 10, &Rov::thrusterForceCallback, this);
   this->m_pressurePub =
     this->m_nh->advertise<sensor_msgs::FluidPressure>("/sensors/pressure",10);
   this->m_timer = this->m_nh->createTimer(ros::Duration(0.1), &Rov::timerCallback, this);
@@ -30,25 +31,17 @@ void gazebo::Rov::Load(physics::ModelPtr _parent, sdf::ElementPtr)
 
 void gazebo::Rov::simulationCallback(const common::UpdateInfo &)
 {
-  this->m_link->AddRelativeForce(this->m_force);
-  this->m_link->AddRelativeTorque(this->m_torque);
+  this->m_pose = this->m_link->GetWorldCoGPose();
 
-  math::Vector3 v_l = this->m_link->GetRelativeLinearVel();
-  math::Vector3 v_a = this->m_link->GetRelativeAngularVel();
-  math::Vector3 F_l = this->c_linearDragCoeff*v_l*v_l.GetAbs();
-  math::Vector3 F_a = this->c_angularDragCoeff*v_a*v_a.GetAbs();
-
-  this->m_link->AddRelativeForce(-F_l);
-  this->m_link->AddRelativeTorque(-F_l);
-
-  this->m_pose = this->m_link->GetWorldCoGPose().pos;
+  this->applyThrusterForces();
+  this->applyFrictionForces();
 }
 
 
 void gazebo::Rov::rovForceCallback(const geometry_msgs::Wrench &_msg)
 {
   // Change sign of force in y, and of torque in y and z
-  math::Vector3 forceMsg(_msg.force.x, -_msg.force.y, _msg.force.z);
+  math::Vector3 forceMsg(_msg.force.x, -_msg.force.y, -_msg.force.z);
   math::Vector3 torqueMsg(_msg.torque.x, -_msg.torque.y, -_msg.torque.z);
 
   this->m_force = forceMsg;
@@ -56,8 +49,49 @@ void gazebo::Rov::rovForceCallback(const geometry_msgs::Wrench &_msg)
 }
 
 
+void gazebo::Rov::thrusterForceCallback(const vortex_msgs::ThrusterForces &_msg)
+{
+  for (int i = 0; i < 8; i++)
+    this->m_thrusterForces[i] = this->c_thrusterLayout[i]*_msg.thrust[i];
+}
+
+
 void gazebo::Rov::timerCallback(const ros::TimerEvent&)
 {
-  this->m_pressure.fluid_pressure = -((50 - this->m_pose.z)*1000*9.810665 + 101300);
+  this->m_pressure.fluid_pressure = ((50 - this->m_pose.pos.z)*1000*9.810665 + 101300);
   this->m_pressurePub.publish(this->m_pressure);
+}
+
+void gazebo::Rov::applyFrictionForces()
+{
+  math::Vector3 v_l = this->m_link->GetRelativeLinearVel();
+  math::Vector3 v_a = this->m_link->GetRelativeAngularVel();
+  math::Vector3 F_l = this->c_linearDragCoeff*v_l*v_l.GetAbs();
+  math::Vector3 F_a = this->c_angularDragCoeff*v_a*v_a.GetAbs();
+
+  this->m_link->AddRelativeForce(-F_l);
+  this->m_link->AddRelativeTorque(-F_a);
+}
+
+void gazebo::Rov::applyRovForces()
+{
+  this->m_link->AddRelativeForce(this->m_force);
+  this->m_link->AddRelativeTorque(this->m_torque);
+}
+
+void gazebo::Rov::applyThrusterForces()
+{
+  double w = this->m_pose.rot.w;
+  double x = this->m_pose.rot.x;
+  double y = this->m_pose.rot.y;
+  double z = this->m_pose.rot.z;
+
+  math::Matrix3 R
+    ((1-2*y*y-2*z*z), 2*(x*y+w*z)    , 2*(x*z-w*y),
+     2*(x*y-w*z)    , (1-2*x*x-2*z*z), 2*(y*z+w*x),
+     2*(x*z+w*y)    , 2*(y*z-w*x)    , (1-2*x*x-2*y*y));
+  math::Matrix3 RI = R.Inverse();
+  for (int i = 0; i < 8; i++)
+    this->m_link->AddForceAtRelativePosition(RI*m_thrusterForces[i],
+c_thrusterPosition[i]);
 }
